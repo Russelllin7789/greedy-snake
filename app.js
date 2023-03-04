242a4ea906f6a952474568e49ea2f24cab3c7aef
Russell Lin <superbr7789@gmail.com>
2023-03-04 22:53:52 +0800

    make snake movable

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const canvas = document.getElementById("canvas");
 // get the drawing context via getContext() method to draw within canvas
 const ctx = canvas.getContext("2d");
 
-console.log("context:", ctx);
-
 const unit = 20;
 let snake = [];
+let direction = "Right";
 
 const createSnake = () => {
   snake = [
@@ -18,14 +17,33 @@ const createSnake = () => {
 
 createSnake();
 
-for (let i = 0; i < snake.length; i++) {
-  if (i === 0) {
-    ctx.fillStyle = "yellowGreen";
-  } else {
-    ctx.fillStyle = "#004B97";
-  }
-  ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+const keepDrawing = () => {
+  console.log("game started");
+  for (let i = 0; i < snake.length; i++) {
+    if (i === 0) {
+      ctx.fillStyle = "yellowGreen";
+    } else {
+      ctx.fillStyle = "#004B97";
+    }
 
-  ctx.strokeStyle = "#fff";
-  ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
-}
+    ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+
+    ctx.strokeStyle = "#fff";
+    ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
+  }
+
+  // change the heading direction for the snake
+  const newSnakeX = snake[0].x;
+  const newSnakeY = snake[0].y;
+  let newHead;
+  if (direction === "Right") {
+    newHead = {
+      x: newSnakeX + unit,
+      y: newSnakeY,
+    };
+  }
+  snake.pop();
+  snake.unshift(newHead);
+};
+
+// const game = setInterval(keepDrawing, 100);
